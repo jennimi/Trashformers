9e2f8713e704c9259523dd46d9ca1a053ac98186
Karyna <87291992+jennimi@users.noreply.github.com>
2025-11-17 19:21:44 +0700

    added animation for enemies

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public EnemyScriptableObject enemyData;
+
     private Transform player;
+    private Animator anim;
 
     void Start()
     {
         player = FindFirstObjectByType<PlayerController>().transform;
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (player == null) return;
 
+        // Calculate direction to player
+        Vector2 direction = (player.position - transform.position).normalized;
+
+        // Update Animator parameters
+        anim.SetFloat("PlayerX", direction.x);
+        anim.SetFloat("PlayerY", direction.y);
+
+        // Move toward player
         transform.position = Vector2.MoveTowards(
             transform.position,
             player.position,
